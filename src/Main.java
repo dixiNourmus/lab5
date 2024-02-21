7cd00fa7a1e850fa1e4683cef7cf5626522dd0a8
dixiNourmus <joshua.berger@talawanda.org>
2024-02-21 13:50:00 -0500

    fdsg

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        double tempF = 0;
+        double tempC = 0;
+        String trash = "";
+
+        System.out.println("Give me temp in C");
+
+        if(in.hasNextDouble()){
+
+            tempC = in.nextDouble();
+            tempF = (tempC * 9/5) + 32;
+            System.out.println("Temp converted: "+tempF);
+
+        }
+        else{
+
+            trash = in.nextLine();
+            in.nextLine();
+            System.out.println("you gave me trash: "+trash);
+            System.exit(0);
+
+        }
+
     }
 }
